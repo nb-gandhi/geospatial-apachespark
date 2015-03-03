2177951b5e6755520922aae4a46bab2539575cf0
nb-gandhi <ngandhi.asu@gmail.com>
2015-03-03 15:28:16 -0700

    Farthest Point_Commit 1

---
+++ src/main/java/geometricfarthestpoints/GeometricFarthestPoint.java
@@ -0,0 +1,10 @@
+package geometricfarthestpoints;
+
+public class GeometricFarthestPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First commit for geometric farthest point!");
+	}
+
+}
